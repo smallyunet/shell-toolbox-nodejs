9674a7901b0c9d036baa7b4c27f1822ad32f0e19
smallyu <wangyudev@qq.com>
2020-12-01 13:47:33 +0800

    v0.0.2

--- index.js
+++ index.js
@@ -2,7 +2,20 @@
 
 const [,, ...args] = process.argv
 
-console.log("hello", args)
+let con1 = args.length <= 0
+    || args[0] == '-h' 
+    || args[0] == '--help'
+
+if (con1) {
+    console.log(`
+        gitpush \t Gernerator a gitpush shell script.
+    `)
+}
+
+else {
+    console.log('Under developing...')
+}
+
 
 
 
