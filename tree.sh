40328b604ae625dbcf445f630412159056aec911
smallyu <dev.wangyu@gmail.com>
2021-08-04 13:26:04 +0800

    Delete tree.sh

--- tree.sh
+++
@@ -1,4 +0,0 @@
-
-tree=$(tree -tf --noreport -I '*~|*.md|*.sh|*.yml' --charset ascii -v $1 |
-        sed -e 's/| \+/  /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1<a href=\"\2\">\4<\/a>:g')
-printf "# Index\n\n${tree}\n" > TREE.md
